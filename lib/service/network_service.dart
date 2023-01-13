d70f00878f6882df76fbfca0935c8a2ed3a34a96
Tamunor <admin@Tamunors-MacBook.local>
2023-01-13 07:28:25 +0100

    added Description text to Task model

--- lib/service/network_service.dart
+++ lib/service/network_service.dart
@@ -10,14 +10,46 @@ class NetworkService {
 
   Future<List<Task>> getTasks() async {
     final now = DateTime.now();
+
+    final descriptionText =
+        "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer rutrum condimentum odio id eleifend. Interdum et malesuada fames ac ante ipsum primis in faucibus. Cras dapibus elit tellus, sit amet porta eros luctus eu. Mauris ullamcorper mauris porttitor, dignissim tortor eget, sollicitudin arcu. Aenean eget vehicula orci. Donec pretium interdum lectus, nec scelerisque sapien finibus at. Nulla at vestibulum nibh, nec accumsan ante. Nulla in efficitur justo, eget imperdiet tellus. Quisque vulputate vel leo sed porttitor.";
+
     return [
-      Task(id: '1', title: 'Read emails from mailbox', dateTime: now),
-      Task(id: '2', title: 'Check latest news on Flutter', dateTime: now),
-      Task(id: '3', title: 'Have a call with Flutter team', dateTime: now),
-      Task(id: '4', title: 'Work on application performance', dateTime: now),
-      Task(id: '5', title: 'Plan work for next week', dateTime: now),
-      Task(id: '6', title: 'Order lunch', dateTime: now),
-      Task(id: '7', title: 'Create an invoice for last month', dateTime: now),
+      Task(
+          id: '1',
+          title: 'Read emails from mailbox',
+          dateTime: now,
+          description: descriptionText),
+      Task(
+          id: '2',
+          title: 'Check latest news on Flutter',
+          dateTime: now,
+          description: descriptionText),
+      Task(
+          id: '3',
+          title: 'Have a call with Flutter team',
+          dateTime: now,
+          description: descriptionText),
+      Task(
+          id: '4',
+          title: 'Work on application performance',
+          dateTime: now,
+          description: descriptionText),
+      Task(
+          id: '5',
+          title: 'Plan work for next week',
+          dateTime: now,
+          description: descriptionText),
+      Task(
+          id: '6',
+          title: 'Order lunch',
+          dateTime: now,
+          description: descriptionText),
+      Task(
+          id: '7',
+          title: 'Create an invoice for last month',
+          dateTime: now,
+          description: descriptionText),
     ];
   }
 }
